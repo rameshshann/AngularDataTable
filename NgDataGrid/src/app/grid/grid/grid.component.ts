ea7083ed33db3e169b124836198c8498528d5677
Rameshkumar S <rameshshann@gmail.com>
2019-09-12 12:11:39 +0530

    global search implemented

--- NgDataGrid/src/app/grid/grid/grid.component.ts
+++ NgDataGrid/src/app/grid/grid/grid.component.ts
@@ -14,27 +14,35 @@ export class GridComponent implements OnInit {
   public pagedItems: any[];
   private element: any = {};
   private allItems: any[];
+  public searchText: any = '';
 
   // tslint:disable-next-line:max-line-length
   constructor(private gridServiceService: GridServiceService, private pagerService: PagerService, private gridFilterPipe: GridFilterPipe) { }
 
   ngOnInit() {
     this.gridServiceService.list().subscribe(res => {
-      res.keys =  Object.keys(res.data[0]);
+      res.keys = Object.keys(res.data[0]);
       this.gridData = res;
-     // this.allItems = this.gridData;
-      this.setPage(1);
+      // this.allItems = this.gridData;
+      this.setPage(1, this.gridData.data);
     });
 
   }
-  setPage(page: number) {
-   // console.log(this.gridFilterPipe.transform(this.gridData.data, this.gridData.data[0].name='tiger', true));
+  setPage(page: number, gridData) {
     // get pager object from service
-     this.pager = this.pagerService.getPager(this.gridData.data.length, page);
-     //console.log(this.pager);
+    this.pager = this.pagerService.getPager(gridData.length, page);
+    //console.log(this.searchByName('Tiger', this.gridData.data));
 
     // get current page of items
-    this.pagedItems = this.gridData.data.slice(this.pager.startIndex, this.pager.endIndex + 1);
+    this.pagedItems = gridData.slice(this.pager.startIndex, this.pager.endIndex + 1);
     //console.log(this.pagedItems);
-}
+  }
+  globalSearch() {
+    let data = {};
+    data['name'] = this.searchText;
+    data['position'] = this.searchText;
+    data['place'] = this.searchText;
+    const searchData = this.gridFilterPipe.transform(this.gridData.data, data, false);
+    this.setPage(1, searchData);
+  }
 }
